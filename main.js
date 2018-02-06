58de19451795249f59cdfe892bf16afb13bdccc4
Saul <digitalh2o@gmail.com>
2018-02-05 20:02:30 -0600

    Initial commit of portfolio

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log("hi");
+
+let sliders = document.querySelectorAll(".work-item");
+console.log(sliders);
+
+// from the Underscore Library
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkScroll(e) {
+  sliders.forEach(slider => {
+    //halfway through div
+    const slideAt =
+      window.scrollY + window.innerHeight - slider.clientHeight / 2;
+    //bottom of the div
+    const bottomOfSlider = slider.offsetTop + slider.clientHeight;
+    const isHalfShown = slideAt > slider.offsetTop;
+    const isNotScrollPass = window.scrollY < bottomOfSlider;
+    if (isHalfShown && isNotScrollPass) {
+      console.log(true);
+      slider.classList.add("active");
+    } else {
+      console.log(false);
+      slider.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkScroll));
